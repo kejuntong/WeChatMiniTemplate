a53e67bc9bc14cf38fe8f0a92c255aa7ac22a394
unknown <unknown@example.com>
2020-05-14 23:50:51 -0400

    add weChat avatar

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ App({
       env: 'template-5fnnl',
       traceUser: true
     })
+
+    let that = this
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.wxUserInfo = res.userInfo
+            }
+          })
+        } else {
+          console.log('微信没有授权')
+        }
+      }
+    })
   },
+
   globalData: {
-    userInfo: null
-  }
+    userInfo: null,
+    wxUserInfo: null
+  },
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,15 +3,15 @@ var fetch = require('../../utils/api').default
 var databse = require('../../utils/clouldDatabase').default
 import {navList} from '../../utils/utils'
 
+const app = getApp()
+
 Page({
     data: {
         navList:navList,
         activeValue:'',
         topicList:[],
         showLoadMore:true,
-
         skip:0,
-
         tab:'',
         limit:5
     },
@@ -58,29 +58,30 @@ Page({
         },() => {
             setTimeout(()=>{
                 if (that.data.showLoadMore) that.getAction()
-                else console.log('show load more', 'false')
+                else console.log('show load more:', 'false')
             },600)
         })
     },
 
     getAction(){
+
+        console.log('打印时间', JSON.stringify(new Date()))
+
         wx.showLoading({title:''})
         let that = this
         databse.queryDb('test_post', null, 
         that.data.limit, that.data.skip,
         (res) => {
-            console.log('query success: ', res.data)
+            console.log('query success:', res.data)
             let data = [...that.data.topicList,...res.data]
             this.setData({
                 topicList:data,
             })
-
             if(res.data.length < that.data.limit){
                 this.setData({
                     showLoadMore:false
                 })
             }
-
             wx.hideLoading()
         },
         (err) => {
